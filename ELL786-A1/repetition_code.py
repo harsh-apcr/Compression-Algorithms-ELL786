75cf121aea79facd4b878b43eef71e969e85bdc5
Saransh Agarwal <74806993+saransh738@users.noreply.github.com>
2022-01-29 18:09:07 +0530

    Update repetition_code.py

--- ELL786-A1/repetition_code.py
+++ ELL786-A1/repetition_code.py
@@ -1,27 +1,40 @@
 
+
+# rep_encode function takes string and integer as an argument and encodes it
+# s is binary string to encode
+# r is the value that is agreed commonly between transmitter and receiver
 # r must preferably an odd number
-# transmitter side
+
 def rep_encode(s, r):
-    # s is binary string to encode
-    # r is the value that is agreed commonly between transmitter and receiver
+    
     output = ""
     for c in s:
         output += c*r
+        
     return output
 
 
-# receiver side
-# decode using majority rule
+# rep_decode function takes string and integer as an argument and decodes it
+# s is binary string to encode
+# r is the value that is agreed commonly between transmitter and receiver
+# decoding using majority rule
+
 def rep_decode(s, r):
+    
     i = 0
     n = len(s)
+    
     output = ""
     while i < n:
-        if s.count('0', i, i+r) > r//2:
+        
+        #if count of 0's in the binary string is more than r/2 then append 0 in the output string
+        if (s.count('0', i, i+r) > r//2):
             output += '0'
+        #else append 1
         else:
-            # even if s.count('0',i,i+r) == r/2 (in case r is even) then '1' is taken
+           
             output += '1'
         i += r
+        
     return output
 
