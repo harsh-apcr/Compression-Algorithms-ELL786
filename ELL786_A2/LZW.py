b0c8604ef892d4271fa9dca0d55a95faeb0c07cc
harsh <harsh2001apcr@gmail.com>
2022-02-18 18:10:44 +0530

    LZW completed

--- ELL786_A2/LZW.py
+++ ELL786_A2/LZW.py
@@ -1,61 +1,63 @@
-def encoding(s):
-    Dict = dict()
+
+# d0 : initial dictionary primed with the alphabet set
+# the alphabet set keys must start from 1 to its length with increments of 1
+def encoding(s, d0):
+    code_table = d0
     n = len(s)
-    for i in range(256):
-        t = ""
-        t += chr(i)
-        Dict[t] = i
-    p =""
-    c =""
-    p += s[0]
-    k = 256
-    list = []
-    for i  in range(n):
-        if (i != n- 1):
-            c += s[i + 1];
-        if (p+c in Dict): 
-            p = p + c
+    p = ''
+    code_stream = []
+    k = len(d0)+1
+    for i in range(n):
+        c = s[i]
+        if p + c not in code_table:
+            code_table[p + c] = k
+            code_stream.append(code_table[p])
+            p = c
+            k += 1
         else:
-            list.append(Dict[p])
-            Dict[p+c] = k
-            k+=1
-            p=c
-        c=""
-    list.append(Dict[p])  
-    
-    return list
+            p += c
+
+    code_stream.append(code_table[p])
+    return code_stream
 
 
-def decoding(v):
-    Dict = dict()
-    k =""
-    for i in range(256):
-        t = ""
-        t += chr(i)
-        Dict[i] = t
-        
+def decoding(v, d0):
+    # invert d0 mapping
+    Dict = {v: k for k, v in d0.items()}
+    # dictionary with keys as integers
+    k = ""
     old = v[0]
-    s=Dict[old]
-    c=""
-    c+=s[0]
-    k+=s
-    count=256
-    for i  in range(len(v)-1):
-        n=v[i+1]
-        if ((n in Dict)==False): 
+    s = Dict[old]
+    c = ""
+    c += s[0]
+    k += s
+    count = len(d0)+1
+    for i in range(len(v) - 1):
+        n = v[i + 1]
+        if n not in Dict:
             s = Dict[old]
             s = s + c
         else:
-            s = Dict[n] 
-        k+=s 
-        c=""
-        c+=s[0]
-        Dict[count] = Dict[old]+c
-        count+=1
+            s = Dict[n]
+        k += s
+        c = ""
+        c += s[0]
+        Dict[count] = Dict[old] + c
+        count += 1
         old = n
     return k
 
-print(encoding("00011100"))
-print(decoding(encoding("00011100")))
-        
-        
+
+# initial dictionary must be same both at the encoder as well at the decoder side
+m = "abababababababa"
+d = {" ": 1,
+     "a": 2,
+     "b": 3,
+     "o": 4,
+     "w": 5}
+
+code = encoding(m, d)
+print(code)
+m_dec = decoding(code, d)
+print(m_dec)
+print(m_dec == m)
